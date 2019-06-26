daca1da6862ed96fee06e593a63377f4385790d3
KYpotato <s0methingfun313@gmail.com>
2019-06-26 13:04:38 +0900

    combine locking.html and unlocking.html into one

---
+++ settings.js
@@ -0,0 +1,12 @@
+const bitcoin = require('bitcoinjs-lib');
+exports.API = {
+    CHAIN_SO : 0,
+    MY_NODE : 1
+};
+
+exports.network = bitcoin.networks.testnet;
+exports.api = module.exports.API.CHAIN_SO;
+
+exports.default_fee = 500;
+
+
